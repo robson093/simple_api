eb2ac5ebf8b3a0a72d1d50c11d17aad7a2cc0a62
robson093 <robert.olejniczak.93@gmail.com>
2025-04-19 16:45:12 +0200

    test project added with fail test

---
+++ src/SimpleApi.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using SimpleApi;
+
+namespace SimpleApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void SumOK()
+    {
+        Calculator calc = new Calculator();
+        int result = calc.Sum(1,2);
+        Assert.Equal(result, 3);
+    }
+
+    [Fact]
+    public void SumError()
+    {
+        Calculator calc = new Calculator();
+        int result = calc.Sum(1,2);
+        Assert.Equal(result, 4);
+    }
+}
---
+++ src/SimpleApi/Calculator.cs
@@ -0,0 +1,6 @@
+namespace SimpleApi
+{
+    public class Calculator{
+        public int Sum(int num1, int num2) => num1+num2;
+    }
+}
