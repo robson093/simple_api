ff780982a9ac534e29f24233e8df3331bb07d131
robson093 <robert.olejniczak.93@gmail.com>
2025-04-19 16:50:38 +0200

    test fix

--- src/SimpleApi.Tests/UnitTest1.cs
+++ src/SimpleApi.Tests/UnitTest1.cs
@@ -12,11 +12,11 @@ public class UnitTest1
         Assert.Equal(result, 3);
     }
 
-    [Fact]
-    public void SumError()
-    {
-        Calculator calc = new Calculator();
-        int result = calc.Sum(1,2);
-        Assert.Equal(result, 4);
-    }
+    // [Fact]
+    // public void SumError()
+    // {
+    //     Calculator calc = new Calculator();
+    //     int result = calc.Sum(1,2);
+    //     Assert.Equal(result, 4);
+    // }
 }
